d9ae2b3d15ce57b25b9be602a7c6c8fa303d28fb
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2018-11-07 23:49:31 +0100

    Fixed formatting

--- kern/monitor.c
+++ kern/monitor.c
@@ -60,14 +60,16 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
   struct Eipdebuginfo info;
   uint32_t * ebp = (uint32_t * ) read_ebp();
-  uint32_t * tmp = (uint32_t *) (*(ebp));
+  const char * eip_format = "        %s:%d: %.*s+%d\n";
+  const char * stack_format = "  ebp %x  eip %x  args %08x %08x %08x %08x %08x\n";
+  uint32_t  eip = *(ebp + 1);
+  cprintf("Stack backtrace:\n");
   while ( ebp != 0 ) {
-    cprintf("ebp %x  eip %x  args %08x %08x %08x %08x %08x\n",ebp,*(ebp + 1),*(ebp + 2),*(ebp + 3),*(ebp + 4),*(ebp + 5),*(ebp + 6));
-    if (debuginfo_eip( *(ebp + 1), &info) == 0){
-      cprintf("       %s:%d: %.*s+%d\n",info.eip_file,info.eip_line,info.eip_fn_namelen,info.eip_fn_name,*(tmp + 1) - info.eip_fn_addr);
+    cprintf(stack_format,ebp,eip,*(ebp + 2),*(ebp + 3),*(ebp + 4),*(ebp + 5),*(ebp + 6));
+    if (debuginfo_eip( eip, &info) == 0) {
+      cprintf(eip_format,info.eip_file,info.eip_line,info.eip_fn_namelen,info.eip_fn_name,eip- info.eip_fn_addr);
     }
     ebp = (uint32_t *) (*(ebp));
-
   }
   return 0;
 }
