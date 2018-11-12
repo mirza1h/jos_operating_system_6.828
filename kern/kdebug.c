921ffc3c02df698f213097a9be15900409b8efc7
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2018-11-12 22:54:04 +0100

    eip_line added

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -181,7 +181,7 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	// Your code here.
   stab_binsearch(stabs, &lline, &rline, N_SLINE, addr);
   if(lline <= rline)
-    info->eip_line = lline-lfile;
+    info->eip_line = stabs[lline].n_value;
   else
     return -1;
 	// Search backwards from the line number for the relevant filename
