89d2c0204978ec6b867e1401288e5066949720f9
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2018-12-11 22:44:48 +0100

    region and icode

--- kern/env.c
+++ kern/env.c
@@ -116,11 +116,14 @@ env_init(void)
 {
 	// Set up envs array
 	// LAB 3: Your code here.
-  for(size_t i = 0; i < NENV; ++i){
-    envs[i].env_id = 0;
-    envs[i].env_link = env_free_list;
-    env_free_list = &envs[i];
+  	size_t length = sizeof(envs)/sizeof(envs[0]);
+    size_t i = 0;	
+  for(size_t i = 0; i < length; ++i){
+    		envs[i].env_id = 0;
+    		envs[i].env_link = &envs[i + 1];
   }
+	envs[i - 1].env_link = NULL;
+	env_free_list = &envs[0];
 	// Per-CPU part of the initialization
 	env_init_percpu();
 }
@@ -175,7 +178,7 @@ env_setup_vm(struct Env *e)
 	//	Can you use kern_pgdir as a template?  Hint: Yes.
 	//	(Make sure you got the permissions right in Lab 2.)
 	//    - The initial VA below UTOP is empty.
-	//    - You do not need to make any more calls to page_alloc.
+	//    - You do not need to make any more calls to page_alloc
 	//    - Note: In general, pp_ref is not maintained for
 	//	physical pages mapped only above UTOP, but env_pgdir
 	//	is an exception -- you need to increment env_pgdir's
@@ -183,10 +186,12 @@ env_setup_vm(struct Env *e)
 	//    - The functions in kern/pmap.h are handy.
 
 	// LAB 3: Your code here.
-//  boot_map_region(e->env_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U | PTE_P);
-//  boot_map_region(e->env_pgdir, UENVS, PTSIZE, PADDR(envs), PTE_U | PTE_P);
-//  boot_map_region(e->env_pgdir, KSTACKTOP - KSTKSIZE, KSTKSIZE, PADDR(bootstack), PTE_W | PTE_P);
-//  boot_map_region(e->env_pgdir, KERNBASE, ROUNDUP(0xffffffff - KERNBASE, PGSIZE) , 0x0, PTE_W | PTE_P);
+  	p->pp_ref++;
+  	e->env_pgdir = page2kva(p);
+  	i = PDX(UTOP);
+  	for(;i < 1024; ++i){
+    		e->env_pgdir[i] = kern_pgdir[i];  
+  	}
 	// UVPT maps the env's own page table read-only.
 	// Permissions: kernel R, user R
 	e->env_pgdir[PDX(UVPT)] = PADDR(e->env_pgdir) | PTE_P | PTE_U;
@@ -269,8 +274,18 @@ region_alloc(struct Env *e, void *va, size_t len)
 {
 	// LAB 3: Your code here.
 	// (But only if you need it for load_icode.)
-	//
-	// Hint: It is easier to use region_alloc if the caller can pass
+	va = ROUNDDOWN(va, PGSIZE);
+  size_t end = ROUNDUP((uintptr_t)(va) + len, PGSIZE);
+  struct PageInfo * page;
+  size_t i = 0;
+  for(; i < end; i+=PGSIZE) {
+    page = page_alloc(0);
+    if(page == NULL)
+      panic("No memory!");
+    page_insert(e->env_pgdir, page, (void *)(va + i), PTE_U | PTE_P | PTE_W);
+    page->pp_ref++;
+  }
+  // Hint: It is easier to use region_alloc if the caller can pass
 	//   'va' and 'len' values that are not page-aligned.
 	//   You should round va down, and round (va + len) up.
 	//   (Watch out for corner-cases!)
@@ -330,11 +345,25 @@ load_icode(struct Env *e, uint8_t *binary)
 	//  What?  (See env_run() and env_pop_tf() below.)
 
 	// LAB 3: Your code here.
-
+  lcr3(PADDR(e->env_pgdir));
+  struct Elf * elfhdr = (struct Elf *) (binary);
+  if(elfhdr->e_magic != ELF_MAGIC)
+    panic("NOT ELF!");
+  e->env_tf.tf_eip = elfhdr->e_entry;
+  struct Proghdr * ph = (struct Proghdr *) ((uint8_t *)elfhdr + elfhdr->e_phoff);
+  struct Proghdr * eph = ph + elfhdr->e_phnum;
+  for(;ph < eph; ++ph){
+  if(ph->p_type != ELF_PROG_LOAD)
+    panic("load_icode()!");
+   region_alloc(e, (void *)ph->p_va,ph->p_memsz);
+   memmove((void *)ph->p_va, binary + ph->p_offset, ph->p_filesz);
+   if(ph->p_memsz > ph->p_filesz)
+     memset((void *)ph->p_va + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+  }
 	// Now map one page for the program's initial stack
 	// at virtual address USTACKTOP - PGSIZE.
-
 	// LAB 3: Your code here.
+  region_alloc(e, (void *)USTACKTOP - PGSIZE, PGSIZE);
 }
 
 //
