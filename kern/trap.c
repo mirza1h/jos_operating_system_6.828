881fa0b3950b9aa3f64057007aa8adf013a44fe7
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2019-01-22 14:30:34 +0100

    spin works

--- kern/trap.c
+++ kern/trap.c
@@ -49,6 +49,12 @@ void t_align();
 void t_mchk();   
 void t_simderr();
 void t_syscall();
+void irq_timer();
+void irq_kbd();
+void irq_serial();
+void irq_spurious();
+void irq_ide();
+void irq_error();
 
 static const char *trapname(int trapno)
 {
@@ -110,6 +116,12 @@ trap_init(void)
   SETGATE(idt[T_MCHK], 0, GD_KT, t_mchk, 0);
   SETGATE(idt[T_SIMDERR], 0, GD_KT, t_simderr, 0);
 	SETGATE(idt[T_SYSCALL], 0, GD_KT, t_syscall, 3);
+	SETGATE(idt[IRQ_OFFSET + IRQ_TIMER], 0, GD_KT, irq_timer, 0);
+	SETGATE(idt[IRQ_OFFSET + IRQ_KBD], 0, GD_KT, irq_kbd, 0);
+	SETGATE(idt[IRQ_OFFSET + IRQ_SERIAL], 0, GD_KT, irq_serial, 0);
+	SETGATE(idt[IRQ_OFFSET + IRQ_SPURIOUS], 0, GD_KT, irq_spurious, 0);
+	SETGATE(idt[IRQ_OFFSET + IRQ_IDE], 0, GD_KT, irq_ide, 0);
+	SETGATE(idt[IRQ_OFFSET + IRQ_ERROR], 0, GD_KT, irq_error, 0);
   // Per-CPU setup 
 	trap_init_percpu();
 }
@@ -227,7 +239,10 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle clock interrupts. Don't forget to acknowledge the
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
-
+	if(tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
+		lapic_eoi();
+		sched_yield();
+	}
 	if(tf->tf_trapno == T_PGFLT)
 		page_fault_handler(tf);
 	else if(tf->tf_trapno == T_BRKPT)
